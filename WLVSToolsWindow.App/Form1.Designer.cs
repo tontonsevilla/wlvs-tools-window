6dbdaebd23a18c3bc1204f48cb817efd08cb9aaa
Winston Sevilla <wsevilla@blastasia.com>
2024-10-08 13:40:32 +0800

    Init project.

---
+++ WLVSToolsWindow.App/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WLVSToolsWindow.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            IsMdiContainer = true;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "WLVS Tools by Tonton";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
